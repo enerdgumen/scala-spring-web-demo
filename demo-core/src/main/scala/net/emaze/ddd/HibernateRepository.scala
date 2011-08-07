40214b58f326ccb750df585ff71a670a801edf48
Mauro Rocchi <maurocchi@gmail.com>
2011-08-07 12:34:35 +0200

    enh: HibernateRepository dependencies preconditions

--- demo-core/src/main/scala/net/emaze/ddd/HibernateRepository.scala
+++ demo-core/src/main/scala/net/emaze/ddd/HibernateRepository.scala
@@ -7,29 +7,36 @@ import org.springframework.orm.hibernate3.HibernateOperations
 
 class HibernateRepository extends Repository {
 
-    @BeanProperty
-    var hibernateOperations: HibernateOperations = _
+    @BeanProperty var hibernateOperations: HibernateOperations = _
 
     override def merge[E <: AnyRef](entity: E) = {
+        requireDependencies
         val merged = hibernateOperations.merge(entity).asInstanceOf[E]
         assert(merged != null, "merged entity cannot be null")
         merged
     }
 
     override def search[E <: AnyRef](query: String, args: String*) = {
+        requireDependencies
         val results = hibernateOperations.find(query, args).asInstanceOf[java.util.List[E]]
         assert(results != null, "results cannot be null")
         JavaConversions.asBuffer(results).toList
     }
 
     override def searchById[E <: AnyRef](clazz: Class[E], id: Serializable) = {
+        requireDependencies
         val entity = hibernateOperations.get(clazz, id).asInstanceOf[E]
         if (entity == null) None else Some(entity)
     }
 
     override def findById[E <: AnyRef](clazz: Class[E], id: Serializable) = {
+        requireDependencies
         val entity = hibernateOperations.get(clazz, id).asInstanceOf[E]
         assert(entity != null, "no entity " + clazz.getSimpleName + " found with id " + id)
         entity
+    }
+
+    private def requireDependencies {
+        require(hibernateOperations != null, "hibernateOperations doesn't injected into object of type " + getClass.getName)
     }
 }
