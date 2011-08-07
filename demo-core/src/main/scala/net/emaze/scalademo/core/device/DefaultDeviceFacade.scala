576e412cf2a9d2c25de45cc58aead36232352f12
Mauro Rocchi <maurocchi@gmail.com>
2011-08-07 12:52:17 +0200

    enh: read only transaction

--- demo-core/src/main/scala/net/emaze/scalademo/core/device/DefaultDeviceFacade.scala
+++ demo-core/src/main/scala/net/emaze/scalademo/core/device/DefaultDeviceFacade.scala
@@ -9,16 +9,17 @@ class DefaultDeviceFacade extends DeviceFacade with Logging {
 
     @BeanProperty
     var deviceService: DeviceService = _
-    
+
     override def create(network: String, ipAddress: String) = {
         info(".create(network=%s, ipAddress=%s)", network, ipAddress)
         require(network != null, "network cannot be null")
         require(ipAddress != null, "ipAddress cannot be null")
-        
+
         val device = deviceService.create(network, ipAddress)
         DeviceResponse(device)
     }
 
+    @Transactional(readOnly = true)
     override def searchAll = {
         info(".searchAll()")
         deviceService.searchAll.map(DeviceResponse.apply)
