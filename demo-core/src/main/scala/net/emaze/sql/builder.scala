2ea6bf6317dfebd49a0814dd3984e90cd45307c9
Mauro Rocchi <maurocchi@emaze.net>
2011-08-12 19:01:49 +0200

    dev: can apply a restriction to a select

--- demo-core/src/main/scala/net/emaze/sql/builder.scala
+++ demo-core/src/main/scala/net/emaze/sql/builder.scala
@@ -1,6 +1,6 @@
 package net.emaze.sql
 
-import org.hibernate.criterion.DetachedCriteria
+import org.hibernate.criterion.{Criterion, Restrictions, DetachedCriteria}
 
 trait Criteriable {
 
@@ -9,22 +9,45 @@ trait Criteriable {
 
 object SELECT {
 
-    def apply(what: WhatSelect) = what
+    def apply(domain: WithDomain) = domain
 }
 
-trait WhatSelect {
-    
-    def FROM(table: String) = new Where(table)
+trait WithDomain {
+
+    def FROM(table: String) = new Domain(table)
+}
+object * extends WithDomain
+
+class Domain(table: String) extends Criteriable {
+
+    def WHERE(restriction: Restriction) = new RestrictedDomain(this, restriction)
+
+    override def toCriteria = DetachedCriteria.forEntityName(table)
 }
 
-object * extends WhatSelect
+class RestrictedDomain(criteriable: Criteriable, restriction: Restriction) extends Criteriable {
 
-class Where(table: String) extends Criteriable {
+    override def toCriteria = criteriable.toCriteria.add(restriction.toCriterion)
+}
 
-    override def toCriteria: DetachedCriteria = DetachedCriteria.forEntityName(table)
+trait Restriction {
+
+    def toCriterion: Criterion
+}
+
+class EqRestriction(field: String, value: Any) extends Restriction {
+
+    override def toCriterion = Restrictions.eq(field, value)
+}
+
+class Field(field: String) {
+
+    def ===(value: Any): Restriction = new EqRestriction(field, value)
 }
 
 object Conversion {
 
     implicit def toCriteria(criteriable: Criteriable): DetachedCriteria = criteriable.toCriteria
+
+    implicit def toField(field: String) = new Field(field)
 }
--- demo-core/src/test/scala/net/emaze/sql/SqlBuilderTest.scala
+++ demo-core/src/test/scala/net/emaze/sql/SqlBuilderTest.scala
@@ -1,8 +1,8 @@
 package net.emaze.sql
 
 import org.junit.{Test, Assert}
-import org.hibernate.criterion.DetachedCriteria
 import net.emaze.sql.Conversion._
+import org.hibernate.criterion.{Restrictions, DetachedCriteria}
 
 class SqlBuilderTest {
 
@@ -10,6 +10,13 @@ class SqlBuilderTest {
     def canDoSelectAllFromATable {
         val expectedCriteria = DetachedCriteria.forEntityName("Table")
         val gotCriteria: DetachedCriteria = SELECT (*) FROM "Table"
+        Assert.assertEquals(expectedCriteria.toString, gotCriteria.toString)
+    }
+
+    @Test
+    def canApplyARestrictionToASelect {
+        val expectedCriteria = DetachedCriteria.forEntityName("Table").add(Restrictions.eq("field", 1))
+        val gotCriteria: DetachedCriteria = SELECT (*) FROM "Table" WHERE ("field" === 1)
         Assert.assertEquals(expectedCriteria.toString, gotCriteria.toString)
     }
 }
